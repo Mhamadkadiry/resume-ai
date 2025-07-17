484fb6ab7ee73ec46fa6cdea8e7416d07490c786
Mohamad <Mhamad.kadiry1@gmail.com>
2025-07-17 16:35:35 +0200

    feat: add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,44 +1,39 @@
-import Link from "next/link";
-import { LogOut, Moon, Settings, User } from "lucide-react";
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+"use client";
+
+import { Moon, Sun } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
 const Navbar = () => {
+  const { setTheme } = useTheme();
   return (
     <nav className="flex p-4 items-center justify-between">
-      collapse button
+      <SidebarTrigger />
       <div className="flex items-center gap-4">
-        <Link href="/">Resume AI</Link>
-        <Moon />
-
         <DropdownMenu>
-          <DropdownMenuTrigger>
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent sideOffset={10}>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <User className="h-[1.2rem] w-[1.2rem] mr-2" />
-              Profile
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
             </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Settings className="h-[1.2rem] w-[1.2rem] mr-2" />
-              Settings
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
             </DropdownMenuItem>
-            <DropdownMenuItem variant="destructive">
-              <LogOut className="h-[1.2rem] w-[1.2rem] mr-2" />
-              Logout
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
